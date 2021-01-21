469d96608df0b69c345e46efbbf3569464ed4c0b
Janasyl <jduishenbaev@gmail.com>
2021-01-21 13:40:45 +0600

    added the sixth  question

--- script.js
+++ script.js
@@ -2,21 +2,25 @@ let results = 0;
 
 let answer1 = prompt('Единицей измерения информации является:')
 if (answer1 == 'бит') {
-	results = results + 5;
+	results = results + 10;
 }
 let answer2 = prompt('Какая система счисления используется при представлении числа в памяти компьютера:')
 if (answer2 == 'двоичная') {
-	results = results + 5;
+	results = results + 10;
 }
 let answer3 = prompt('Какие из перечисленных ниже устройств являются устройствами ввода')
 if (answer3 == 'клавиатура') {
-	results = results + 5;
+	results = results + 10;
 }
 let answer4 = prompt('Обработку данных производит:')
 if (answer4 == 'процессор') {
-	results = results + 5;
+	results = results + 10;
 }
 let answer5 = prompt('Какое из устройств наиболее отрицательно влияет на здоровье человека')
 if (answer5 == 'дисплей') {
-	results = results + 5;
+	results = results + 10;
+}
+let answer6 = prompt('Для просмотра содержимого файловой системы используется:')
+if (answer6 == 'Проводник') {
+	results = results + 10;
 }
